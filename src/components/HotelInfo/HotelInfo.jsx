db68fe16b1c1c9969a199694286bf09bb28ed048
sachinupadhyay268 <sachinupadhyay268@gmail.com>
2024-12-30 02:57:36 +0530

    image

--- src/components/HotelInfo/HotelInfo.jsx
+++ src/components/HotelInfo/HotelInfo.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './HotelInfo.css';
+import backgroundImage from '../../assets/secondpagebg.jpg';
 
-const HotelInfo = ({ name }) => (
-  <div className="hotel-info">
-    <h2>About {name}</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel dignissimos, nulla aliquam assumenda...
-    </p>
-  </div>
-);
+const HotelHeader = () => {
+  return (
+    <div className="hotel-header" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="header-overlay">
+        <div className="back-button">
+          &#8592;
+        </div>
+        <div className="hotel-details">
+          <h1>The Peninsula Hotel</h1>
+          <div className="location-rating">
+            <span>
+              &#128205; Mumbai, India
+            </span>
+            <span>
+              &#9733; 4.6
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HotelInfo;
+export default HotelHeader;
